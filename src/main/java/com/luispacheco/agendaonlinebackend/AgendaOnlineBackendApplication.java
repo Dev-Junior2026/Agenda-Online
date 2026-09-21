b6bb8436510f7e25f63b0efbd20f7b874f52480e
dev luis pacheco <lapr271090@gmail.com>
2026-09-21 16:37:45 +0200

    Commit inicial: backend de Agenda-Online

---
+++ src/main/java/com/luispacheco/agendaonlinebackend/AgendaOnlineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.luispacheco.agendaonlinebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaOnlineBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaOnlineBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luispacheco/agendaonlinebackend/AgendaOnlineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luispacheco.agendaonlinebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaOnlineBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
